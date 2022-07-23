f0548d6036c6925e233224b8c257179061440691
adityamavle <89915132+adityamavle@users.noreply.github.com>
2022-07-24 00:31:13 +0530

    Update file_converter.py

--- file_converter.py
+++ file_converter.py
@@ -6,46 +6,44 @@ import os
 import pandas as pd
 import PyPDF2
 
+class fileConverter:
+    path = ""
 
-# def convert_text1():
-# text1 = textract.process(  # filepath of the file to be converted)
-# text2=text1.decode()
-# text_file=open(  # file path on which you want to write the text)
-# text_file.write(text2)
-# text_file.close()
+    def __init__(self, p):
+        self.path = p
 
+    def convert_tostr(self):
+        if self.path.endswith(".docx"):
+            doc = docx.Document(self.path)
+            fullText = []
+            for para in doc.paragraphs:
+                fullText.append(para.text)
+                text = '\n'.join(fullText)
+                print(text)
+        elif self.path.endswith(".pdf"):
+            pdfFileObj = open(self.path, 'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=False)
+            pageObj = pdfReader.getPage(0)
+            text = pageObj.extract_text()
+            #print(text)
+        elif self.path.endswith(".csv"):
+            df1 = pd.read_csv(self.path)
+            text = df1.to_string()
+            #print(text)
+        elif self.path.endswith(".xlsx"):
+            df1 = pd.read_excel(self.path)
+            text = df1.to_string()
+            #print(text)
+        elif (self.path.endswith(".jpg") or self.path.endswith(".png") or self.path.endswith(".gif") or self.path.endswith(".jpeg") or self.path.endswith(".bmp") or self.path.endswith(".eps") or
+              self.path.endswith(".raw") or
+              self.path.endswith(".cr2") or self.path.endswith(".nef") or self.path.endswith(".orf") or self.path.endswith(".sr2")):
 
-def convert_tostr(in_file):
-    if in_file.endswith(".docx"):
-        doc = docx.Document(in_file)
-        fullText = []
-        for para in doc.paragraphs:
-          fullText.append(para.text)
-          text='\n'.join(fullText)
-          print(text)
-    elif in_file.endswith(".pdf"):
-        pdfFileObj = open(in_file, 'rb')
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObj,strict=False)
-        pageObj = pdfReader.getPage(0)
-        text=pageObj.extract_text()
-        print(text)  
-    elif in_file.endswith(".csv"):
-        df1 = pd.read_csv(in_file)
-        text=df1.to_string()
-        print(text)
-    elif in_file.endswith(".xlsx"):
-        df1 = pd.read_excel(in_file)
-        text=df1.to_string()
-        print(text)
-    elif (in_file.endswith(".jpg") or in_file.endswith(".png") or in_file.endswith(".gif") or in_file.endswith(".jpeg") or in_file.endswith(".bmp") or in_file.endswith(".eps") or
-    in_file.endswith(".raw") or
-    in_file.endswith(".cr2") or in_file.endswith(".nef") or in_file.endswith(".orf") or in_file.endswith(".sr2")):
-    
-      pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
-
-      text = pytesseract.image_to_string(Image.open(in_file))
-      print(text)
-
-
-convert_tostr('C:\\Backup Project\\1.pdf')
-convert_tostr('C:\\Users\\adity\\Downloads\\itotext.png')
+            pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
+            text = pytesseract.image_to_string(Image.open(self.path))
+            #print(text)
+        return text
+if __name__ == "__main__":
+    f = fileConverter('C:\\Users\\adity\\Downloads\\itotext.png')
+    f.convert_tostr()
+    # convert_tostr('C:\\Backup Project\\1.pdf')
+    # convert_tostr('C:\\Users\\adity\\Downloads\\itotext.png')
